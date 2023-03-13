b95b1b16ec54bc9394ef6633ec26e8b0be0b56be
Uliana <ulianaastra1@gmail.com>
2023-03-13 18:59:15 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const text = document.querySelector(".color");
+
+button.addEventListener("click", buttonClickHandler);
+
+function buttonClickHandler(event) {
+  const currentColor = getRandomHexColor();
+  text.textContent = currentColor;
+  body.style.backgroundColor = currentColor;
+}
